7de3c790477e7faae1bbb12edc31df5ca4c5d9dd
Daniel Xiong <dxiong2000@gmail.com>
2021-06-27 20:31:02 -0700

    implemented !add feature

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ with open('./secret.txt', 'r') as secret:
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
+file_list = []
+
+def getFileList():
+    return os.listdir('./clips/')
 
 def getErrorEmbed(desc):
     """
@@ -42,6 +46,7 @@ def getHelpEmbed():
         colour = discord.Colour.orange()
     )
     embed.add_field(name='!play <clip name>', value='Play a clip from the soundboard.', inline=False)
+    embed.add_field(name='!add', value='Type !add and upload a .mp3 file in the same message to add a new sound clip.', inline=False)
     embed.add_field(name='!list', value='List all available sound clips.', inline=False)
     return embed
 
@@ -57,12 +62,28 @@ def getListEmbed(file_list):
     """
     embed = discord.Embed(
         title = 'Sound Clips',
-        colour = discord.Colour.green()
+        colour = discord.Colour.blue()
     )
     for f in file_list:
         embed.add_field(name=f[:-4], value=f'!play {f[:-4]}', inline=False)
     return embed
 
+def getAddSuccessEmbed(filename):
+    """
+    Generates a !add success embedded message
+
+    Args:
+        filename (str): name of file added
+        
+    Returns:
+        Embed: success embed
+    """
+    embed = discord.Embed(
+        title = 'Add Success',
+        description = f'Added sound clip: {filename}',
+        colour = discord.Colour.green()
+    )
+    return embed
 
 @client.event
 async def on_ready():
@@ -75,7 +96,7 @@ async def on_ready():
 async def on_message(message):
     """
     Parses commands sent to bot
-    https://discordpy.readthedocs.io/en/stable/api.html#discord.VoiceClient.play
+    https://discordpy.readthedocs.io/en/stable/api.html#voice-related
     Args:
         message (Message): Discord API Message object (https://discordpy.readthedocs.io/en/stable/api.html#message)
     """
@@ -102,21 +123,42 @@ async def on_message(message):
             channel = user.voice.channel
 
             try:
-                voice = await channel.connect()
-                voice.play(discord.FFmpegPCMAudio(executable="C:/FFmpeg/ffmpeg/ffmpeg.exe", source=f'./clips/{soundclip_file}'), after=lambda e: print(f'Played {soundclip_file}', e))
-                while voice.is_playing():
-                    pass
-                await voice.disconnect()
-                voice.cleanup()
+                if soundclip_file in getFileList():
+                    voice = await channel.connect()
+                    voice.play(discord.FFmpegPCMAudio(executable="C:/FFmpeg/ffmpeg/ffmpeg.exe", source=f'./clips/{soundclip_file}'), after=lambda e: print(f'Played {soundclip_file}', e))
+                    while voice.is_playing():
+                        pass
+                    await voice.disconnect()
+                    voice.cleanup()
+                else:
+                    await message.channel.send(f'{user.mention}\n', embed=getErrorEmbed(f'The clip "{soundclip_name}" does not exist. Use !list to see available sound clips.'))
+                    return
             except discord.errors.ClientException:
                 print("Bot is already in a voice channel.")
-            
+            return         
         else:
             await message.channel.send(f'{user.mention}\n', embed=getErrorEmbed('Wrong usage of !play.\nCorrect usage: !play <clip name>.'))
             return
+    elif msg.startswith('!add'): # feature to add new clips
+        msg = msg[4:]
+        if len(msg) == 0 and len(message.attachments) == 1:
+            if message.attachments[0].filename[-4:] == '.mp3':
+                if message.attachments[0].filename in getFileList():
+                    await message.channel.send(f'{user.mention}\n', embed=getErrorEmbed('File name already in use. Change the file name and try again.'))
+                    return
+                file = message.attachments[0]
+                await file.save(fp=f'./clips/{message.attachments[0].filename}')
+                
+                await message.channel.send(f'{user.mention}\n', embed=getAddSuccessEmbed(message.attachments[0].filename[:-4]))
+                return
+            else:
+                await message.channel.send(f'{user.mention}\n', embed=getErrorEmbed('Unsupported file format. Please only upload .mp3 files.'))
+                return
+        else:
+            await message.channel.send(f'{user.mention}\n', embed=getErrorEmbed('Wrong usage of !add.\nType !add and upload a .mp3 file in the same message.'))
+            return
     elif msg.startswith('!list'): # print a list of sound files
-        file_list = os.listdir('./clips/')
-        await message.channel.send(f'{user.mention}\n', embed=getListEmbed(file_list))
+        await message.channel.send(f'{user.mention}\n', embed=getListEmbed(getFileList()))
         return
     elif msg.startswith('!help'): # help menu
         await message.channel.send(f'{user.mention}\n', embed=getHelpEmbed())
