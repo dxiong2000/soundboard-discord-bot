b0e325f929795d66535478834697312245ab235e
Daniel Xiong <dxiong2000@gmail.com>
2021-06-27 18:43:54 -0700

    finished main functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 # gets secret key
 bot_token = ''
@@ -30,6 +31,30 @@ def getErrorEmbed(desc):
     embed.set_footer(text='Type !help for a list of commands.')
     return embed
 
+def getHelpEmbed():
+    """
+    Generates a Discord help embedded message
+
+    Returns:
+        Embed: Help embed
+    """
+    embed = discord.Embed(
+        title = 'Soundboard Help',
+        colour = discord.Colour.orange()
+    )
+    embed.add_field(name='!play <clip name>', value='Play a clip from the soundboard.', inline=False)
+    embed.add_field(name='!list', value='List all available sound clips.', inline=False)
+    return embed
+
+def getListEmbed(file_list):
+    embed = discord.Embed(
+        title = 'Sound Clips',
+        colour = discord.Colour.green()
+    )
+    for f in file_list:
+        embed.add_field(name=f[:-4], value=f'!play {f[:-4]}', inline=False)
+    return embed
+
 
 @client.event
 async def on_ready():
@@ -42,6 +67,7 @@ async def on_ready():
 async def on_message(message):
     """
     Parses commands sent to bot
+    https://discordpy.readthedocs.io/en/stable/api.html#discord.VoiceClient.play
     Args:
         message (Message): Discord API Message object (https://discordpy.readthedocs.io/en/stable/api.html#message)
     """
@@ -60,15 +86,34 @@ async def on_message(message):
         if len(msg) > 0 and msg[0] == ' ': # input validation
             soundclip_name = msg[1:]
             soundclip_file = soundclip_name + '.mp3'
-            print(soundclip_file)
             
+            if not user.voice or not user.voice.channel:
+                await message.channel.send(f'{user.mention}\n', embed=getErrorEmbed('You must be in a voice channel to use this command.'))
+                return
+                
+            channel = user.voice.channel
+
+            try:
+                voice = await channel.connect()
+                voice.play(discord.FFmpegPCMAudio(executable="C:/FFmpeg/ffmpeg/ffmpeg.exe", source=f'./clips/{soundclip_file}'), after=lambda e: print(f'Played {soundclip_file}', e))
+                while voice.is_playing():
+                    pass
+                await voice.disconnect()
+                voice.cleanup()
+            except discord.errors.ClientException:
+                print("Bot is already in a voice channel.")
             
         else:
             await message.channel.send(f'{user.mention}\n', embed=getErrorEmbed('Wrong usage of !play.\nCorrect usage: !play <clip name>.'))
             return
+    elif msg.startswith('!list'):
+        file_list = os.listdir('./clips/')
+        await message.channel.send(f'{user.mention}\n', embed=getListEmbed(file_list))
+        return
+    elif msg.startswith('!help'):
+        await message.channel.send(f'{user.mention}\n', embed=getHelpEmbed())
+        return
         
-    
-    
 
 client.run(bot_token)
         
